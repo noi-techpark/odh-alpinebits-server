7c709050883159cc0d252630e5ffc70b954d1dad
gappc <gapp.christian@gmail.com>
2020-07-18 19:21:11 +0200

    Implement tests for XmlMiddlewareBuilder

--- application-common/utils/src/main/java/it/bz/opendatahub/alpinebitsserver/application/common/utils/XmlMiddlewareBuilder.java
+++ application-common/utils/src/main/java/it/bz/opendatahub/alpinebitsserver/application/common/utils/XmlMiddlewareBuilder.java
@@ -6,6 +6,7 @@
 
 package it.bz.opendatahub.alpinebitsserver.application.common.utils;
 
+import it.bz.opendatahub.alpinebits.middleware.Context;
 import it.bz.opendatahub.alpinebits.middleware.Key;
 import it.bz.opendatahub.alpinebits.middleware.Middleware;
 import it.bz.opendatahub.alpinebits.xml.JAXBObjectToXmlConverter;
@@ -27,19 +28,48 @@ public final class XmlMiddlewareBuilder {
         // Empty
     }
 
-    public static <T> Middleware buildXmlToObjectConvertingMiddleware(Key<T> key, String version) {
-        Schema schema = XmlValidationSchemaProvider.buildXsdSchemaForAlpineBitsVersion(version);
+    /**
+     * Build a {@link Middleware} that converts a XML to an object of type <code>T</code>.
+     *
+     * @param key               The converted object is put in the middleware {@link Context} using this key.
+     * @param alpineBitsVersion The AlpineBits version used for XML validation.
+     * @param <T>               The type of the converted object.
+     * @return A Middleware that converts a XML to an object of type <code>T</code>.
+     */
+    public static <T> Middleware buildXmlToObjectConvertingMiddleware(Key<T> key, String alpineBitsVersion) {
+        validateParams(key, alpineBitsVersion);
+
+        Schema schema = XmlValidationSchemaProvider.buildXsdSchemaForAlpineBitsVersion(alpineBitsVersion);
         XmlToObjectConverter<T> converter = new JAXBXmlToObjectConverter.Builder<>(key.getType()).schema(schema).build();
         return new XmlRequestMappingMiddleware<>(converter, key);
     }
 
-    public static <T> Middleware buildObjectToXmlConvertingMiddleware(Key<T> key, String version) {
-        Schema schema = XmlValidationSchemaProvider.buildXsdSchemaForAlpineBitsVersion(version);
+    /**
+     * Build a {@link Middleware} that converts an object of type <code>T</code> to XML.
+     *
+     * @param key               The object to convert is read from the middleware {@link Context} using this key.
+     * @param alpineBitsVersion The AlpineBits version used for XML validation.
+     * @param <T>               The type of the object to convert.
+     * @return A Middleware that converts an object of type <code>T</code> to a XML.
+     */
+    public static <T> Middleware buildObjectToXmlConvertingMiddleware(Key<T> key, String alpineBitsVersion) {
+        validateParams(key, alpineBitsVersion);
+
+        Schema schema = XmlValidationSchemaProvider.buildXsdSchemaForAlpineBitsVersion(alpineBitsVersion);
         ObjectToXmlConverter converter = new JAXBObjectToXmlConverter.Builder()
                 .schema(schema)
                 .prettyPrint(true)
                 .build();
         return new XmlResponseMappingMiddleware<>(converter, key);
+    }
+
+    private static <T> void validateParams(Key<T> key, String version) {
+        if (key == null) {
+            throw new IllegalArgumentException("The context key must not be null");
+        }
+        if (version == null) {
+            throw new IllegalArgumentException("The version must not be null");
+        }
     }
 
 }
