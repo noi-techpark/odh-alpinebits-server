5ba9709b84b9d0e77fed70f8b38fb1ea73adca30
henri <henri@moosbauer.com>
2023-06-05 15:32:01 +0200

    make reuse compliant

--- odh-backend/api/src/main/java/it/bz/opendatahub/alpinebitsserver/odh/backend/odhclient/client/AuthenticatedOdhClient.java
+++ odh-backend/api/src/main/java/it/bz/opendatahub/alpinebitsserver/odh/backend/odhclient/client/AuthenticatedOdhClient.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
