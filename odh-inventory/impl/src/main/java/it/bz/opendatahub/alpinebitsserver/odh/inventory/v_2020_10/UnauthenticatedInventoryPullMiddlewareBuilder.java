8895d30a898132b33a8645075a0927de92423f0e
henri <henri@moosbauer.com>
2023-06-08 11:19:15 +0200

    change reuse licensing to MPL-2.0

--- odh-inventory/impl/src/main/java/it/bz/opendatahub/alpinebitsserver/odh/inventory/v_2020_10/UnauthenticatedInventoryPullMiddlewareBuilder.java
+++ odh-inventory/impl/src/main/java/it/bz/opendatahub/alpinebitsserver/odh/inventory/v_2020_10/UnauthenticatedInventoryPullMiddlewareBuilder.java
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MPL-2.0
 
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
